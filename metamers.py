005a2291764c2c8ff69f4f908a7a6befc4727d0c
vnika <vskibicka@gmail.com>
2023-06-01 14:34:42 +0200

    activation exploration

--- metamers.py
+++ metamers.py
@@ -38,8 +38,9 @@ def optimise_metamer(input_img, model, orig_activation, hs_num, n_steps, upward_
     for _ in (pbar:= tqdm(range(n_steps))):
         outputs_t = model(input_img)
         hs = torch.square(torch.add(outputs_t.hidden_states[hs_num], -orig_activation[hs_num]))
-        print(f'hs {hs}')
         loss = torch.mul(torch.norm(hs, dim=(1,2), p=2), 1/(torch.norm(orig_activation[hs_num])+1e-8))
+        print(loss)
+        print(f'{loss.is_cuda}')
 
         loss.backward()
         # grads = input_img.grad
