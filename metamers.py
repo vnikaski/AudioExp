b1a7c0d0c04bd03b248befcab2bb4a94d9967eff
vnika <vskibicka@gmail.com>
2023-06-02 02:25:32 +0200

    activation exploration

--- metamers.py
+++ metamers.py
@@ -49,10 +49,10 @@ def optimise_metamer(input_img, model, orig_activation, hs_num, n_steps, upward_
         if j==0:
             print(prev_loss)
 
-        if loss==0:
+        if loss[0]==0:
             return input_img, loss[0]
 
-        if loss>prev_loss:
+        if loss[0]>prev_loss[0]:
             if upward_count>=upward_lim:
                 input_img = torch.nn.Parameter(prev_inp.detach().clone().requires_grad_(True).to(device))
                 #input_img = prev_inp.detach().clone().requires_grad_(True)
