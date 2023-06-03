3232d1701336aae3dbb564f7a531f841da5e30cf
vnika <vskibicka@gmail.com>
2023-06-03 03:21:38 +0200

    bias exploration

--- bias_exploration.py
+++ bias_exploration.py
@@ -9,6 +9,8 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import argparse
 
+from tqdm import tqdm
+
 input_shape = (256,256,1)
 target_shape = 256*256
 
@@ -27,9 +29,13 @@ df = validated.dropna().reset_index(drop=True)
 
 datapath = os.path.join(datapath, 'cochleagrams')
 
+print('loading data ...')
+
 train_X = np.empty((len(df), *input_shape))
 for index, fname in enumerate(df['path']):
     train_X[index] = np.load(os.path.join(datapath, fname+'.npy')).reshape(input_shape)
+
+print('... data loaded :)')
 
 df = df.drop([2437]).reset_index(drop=True)
 train_X = np.delete(train_X, 2437, 0)
@@ -41,12 +47,15 @@ else:
 
 assert len(np.where(pd.isna(train_X))[0]) == 0
 
+print('base')
+
 layer = 'base'
 
 clf = LogisticRegression(max_iter=1000)
 scores = cross_val_score(clf, train_X.reshape((train_X.shape[0], target_shape)), y, cv=10)
 np.save(os.path.join(args.savepath, f'{args.mode}_{layer}.npy'), scores)
 
+print('loading model...')
 
 model = Kell2018(input_shape=(256,256,1), wout_shape = 589, gout_shape = 43, pretrained=True)
 w_weights = np.load('models/network_weights_word_branch.npy', allow_pickle=True, encoding='latin1')
@@ -64,10 +73,13 @@ for lname in list(g_weights.keys()):
         g_weights[lname]['b']
     ])
 
+print('...model loaded :)')
+
 
 layers = ['cochleagram', 'relu1', 'max_pool1', 'relu2', 'max_pool2', 'relu3', 'relu4_W', 'relu5_W', 'avg_pool5_W', 'fc6_W', 'fctop_W', 'relu4_G', 'relu5_G', 'avg_pool5_G', 'fc6_G', 'fctop_G']
 
-for layer in layers:
+for layer in (pbar:= tqdm(layers)):
+    pbar.set_description(f"Layer {layer}")
     maxim = Maximiser()
     maxim.init_submodel(model=model, out_layer_name=layer)
     rep_X = maxim.submodel.predict(train_X)
