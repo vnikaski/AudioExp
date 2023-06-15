db25e5f9d5ac89c7aa2b44ec5948fc9ad3e6f2e4
vnika <vskibicka@gmail.com>
2023-06-15 15:51:30 +0200

    metamers

--- metamers.py
+++ metamers.py
@@ -21,7 +21,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def rescale_gradients(gradients):
-    return gradients/(torch.sqrt(torch.mean(input=torch.square(gradients), dim=[1,2], keepdims=True))+1e-6)
+    return gradients/(torch.sqrt(torch.mean(input=torch.square(gradients), dim=[0,1], keepdims=True))+1e-6)
 
 """
 def lr_factor(step, warmup, total_steps):
@@ -49,8 +49,8 @@ def get_data_sample(i):
 def optimise_metamer(input_img, model, orig_activation, hs_num, n_steps, upward_lim=16, reduce_factor=0.5, prev_loss=None, save_dir=None):
     CHANGE_RATE = False
     if prev_loss is None:
-        prev_loss=np.inf
-    upward_count=0
+        prev_loss=np.full(input_img.shape[0], fill_value=np.inf)
+    upward_count=np.full(input_img.shape[0], fill_value=0)
 
     #input_img = torch.nn.Parameter(input_img.to(device))
     input_img = torch.nn.Parameter(input_img.detach().clone().requires_grad_(True).to(device))
@@ -65,26 +65,33 @@ def optimise_metamer(input_img, model, orig_activation, hs_num, n_steps, upward_
         hs = torch.add(outputs_t.hidden_states[hs_num], -orig_activation[hs_num])
         loss = torch.mul(torch.norm(hs, dim=(1,2), p=2), 1/(torch.norm(orig_activation[hs_num], p=2)+1e-8))
 
-        loss.backward()
-        grads = input_img.grad
-        with torch.no_grad():
-            input_img -= lr * lr_factor(step=j, warmup=256, total_steps=n_steps) * rescale_gradients(grads)
+        lfac = lr_factor(step=j, warmup=256, total_steps=n_steps)
+        new_input_img = input_img.detach().clone()
+        for m in range(len(loss)):
+            if m!=len(loss)-1:
+                loss[m].backward(inputs=input_img, retain_graph=True)
+            else:
+                loss[m].backward(inputs=input_img, retain_graph=False)
+            grads = input_img.grad
+            with torch.no_grad():
+                new_input_img[m] -= lr * lfac * rescale_gradients(grads[m])
         #optimizer.step()
-
+        """"
         if loss[0]==0:
             return input_img, loss[0]
-
-        if loss>prev_loss:
-            if upward_count>=upward_lim:
-                input_img = torch.nn.Parameter(prev_inp.detach().clone().requires_grad_(True).to(device))
-                #input_img = prev_inp.detach().clone().requires_grad_(True)
-                upward_count = 0
+        """
+        for m in range(len(loss)):
+            if loss[m] >= prev_loss[m]:
+                if upward_count[m] >= upward_lim:
+                    input_img[m] = torch.nn.Parameter(prev_inp[m].detach().clone().requires_grad_(True).to(device))
+                    #input_img = prev_inp.detach().clone().requires_grad_(True)
+                    upward_count[m] = 0
+                else:
+                    upward_count[m] += 1
             else:
-                upward_count += 1
-        else:
-            upward_count=0
-            prev_loss = loss.detach().clone()
-            prev_inp = input_img.detach().clone()
+                upward_count[m]=0
+                prev_loss[m] = loss[m].detach().clone()
+                prev_inp[m] = input_img[m].detach().clone()
 
         input_img = torch.Tensor(np.clip(input_img.detach().cpu().numpy(), a_min=-1.5, a_max=1.5))
         input_img = torch.nn.Parameter(input_img.requires_grad_(True).to(device))
@@ -95,7 +102,7 @@ def optimise_metamer(input_img, model, orig_activation, hs_num, n_steps, upward_
             CHANGE_RATE = True
 
         #pbar.set_description(f'loss: {loss[0]}, lr: {optimizer.param_groups[0]["lr"]}, up: {upward_count}')
-        pbar.set_description(f'loss: {loss[0]}, lr: {lr * lr_factor(step=j, warmup=256, total_steps=n_steps)}, up: {upward_count}')
+        pbar.set_description(f'loss: {["{0:.4f}".format(loss[m]) for m in range(len(loss))]}, lr: {lr * lr_factor(step=j, warmup=256, total_steps=n_steps)}, up: {upward_count}')
 
     """
     for j in (pbar:= tqdm(range(n_steps))):
@@ -140,8 +147,8 @@ def get_AST_metamers(sample, model, save_dir, hidden_states, n_mets=1):
     metamers = [torch.tensor(np.random.random_sample((n_mets, *sample.shape[1:])), dtype=torch.float32) for _ in range(N_HS)]
     sample_activation = model(sample).hidden_states
     for i in hidden_states:
-        input_img = torch.tensor(np.random.random_sample((n_mets, sample.shape[1:])), dtype=torch.float32)
-        loss=np.inf
+        input_img = torch.tensor(np.random.random_sample((n_mets, *sample.shape[1:])), dtype=torch.float32)
+        loss=np.full(input_img.shape[0], fill_value=np.inf)
         input_img, loss = optimise_metamer(
             input_img=input_img,
             model=model,
@@ -160,6 +167,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--savepath')
 parser.add_argument('--hiddenstates', default='all')
 parser.add_argument('--id', default=0)
+parser.add_argument('--nmets', default=1)
 args = parser.parse_args()
 
 ID = int(args.id)
@@ -183,7 +191,7 @@ for param in model.parameters():
     param.requires_grad = False
 sample = sample.to(device)
 
-metamers = get_AST_metamers(sample, model, save_dir=args.savepath, hidden_states=hs)
+metamers = get_AST_metamers(sample, model, save_dir=args.savepath, hidden_states=hs, n_mets=int(args.nmets))
 
 plt.figure()
 for i in range(N_HS):
